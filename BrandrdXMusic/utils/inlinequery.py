7a60b3cd0fde5a3d9082f89c83de070eaca5f3fb
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-02-14 17:26:40 +0530

    Add files via upload

---
+++ BrandrdXMusic/utils/inlinequery.py
@@ -0,0 +1,44 @@
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pᴀᴜsᴇ",
+            description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Rᴇsᴜᴍᴇ",
+            description=f"🍃ʀᴇsᴜᴍᴇ🍃 ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Sᴋɪᴩ",
+            description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="Eɴᴅ",
+            description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/end"),
+        ),
+        InlineQueryResultArticle(
+            title="Sʜᴜғғʟᴇ",
+            description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="Lᴏᴏᴩ",
+            description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/df8fa2868f8a277718a47.jpg",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
